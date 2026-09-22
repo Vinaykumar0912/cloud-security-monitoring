82946b7b608939c0e3d71fe631e6a0f15ec68ff0
Vinay Kumar <dixitvinaykumar7@gmail.com>
2026-09-22 19:02:49 +0530

    Configure Vercel API proxy

--- cloud_security_monitoring_frontend/src/api/constants.js
+++ cloud_security_monitoring_frontend/src/api/constants.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:8080";
-export const API_URL = "http://16.176.183.13:8080";
+// export const API_URL = "http://16.176.183.13:8080";
+export const API_URL = "/api";

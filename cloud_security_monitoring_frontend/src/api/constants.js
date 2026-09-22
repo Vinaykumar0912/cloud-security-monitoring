55d97b9b24d67c1068d441d227f0f6d0e36cb5ff
Vinay Kumar <dixitvinaykumar7@gmail.com>
2026-09-22 19:16:18 +0530

    Use HTTPS backend endpoint

--- cloud_security_monitoring_frontend/src/api/constants.js
+++ cloud_security_monitoring_frontend/src/api/constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8080";
 // export const API_URL = "http://16.176.183.13:8080";
-export const API_URL = "/api";
+export const API_URL = "https://industries-materials-kate-emotions.trycloudflare.com";

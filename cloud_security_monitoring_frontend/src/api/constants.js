512674c04507a46d45cd4655b09140545061d036
Vinay Kumar <dixitvinaykumar7@gmail.com>
2026-09-22 19:31:07 +0530

    Revert "Configure Vercel API proxy"
    This reverts commit 82946b7b608939c0e3d71fe631e6a0f15ec68ff0.

--- cloud_security_monitoring_frontend/src/api/constants.js
+++ cloud_security_monitoring_frontend/src/api/constants.js
@@ -1,3 +1,2 @@
 // export const API_URL = "http://localhost:8080";
-// export const API_URL = "http://16.176.183.13:8080";
-export const API_URL = "/api";
+export const API_URL = "http://16.176.183.13:8080";
